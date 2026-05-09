f0ae3301f301e9c44d2be38f3fead3816b333794
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-09 03:24:09 -0300

    Add AchouFood/Scenes/HomeDelivery/HomeDeliveryCoordinator.swift

---
+++ AchouFood/Scenes/HomeDelivery/HomeDeliveryCoordinator.swift
@@ -0,0 +1,10 @@
+//
+//  PlaceDetailCoordinatorProtocol.swift
+//  AchouFood
+//
+//  Created by Arthur Rios on 17/12/25.
+//
+
+public protocol HomeDeliveryCoordinator: AnyObject {
+    func navigateToPlaceDetail(place: Place)
+}
