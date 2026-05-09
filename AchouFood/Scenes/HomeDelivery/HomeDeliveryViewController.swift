1b39a5e38a6823ae4016b567010c93a5979af5a7
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-09 03:24:13 -0300

    Add AchouFood/Scenes/HomeDelivery/HomeDeliveryViewController.swift

---
+++ AchouFood/Scenes/HomeDelivery/HomeDeliveryViewController.swift
@@ -0,0 +1,84 @@
+//
+//  HomeDeliveryViewController.swift
+//  AchouFood
+//
+//  Created by Arthur Rios on 20/10/25.
+//
+
+import UIKit
+import SnapKit
+
+class HomeDeliveryViewController: UIViewController {
+    
+    private var homeView: HomeDeliveryView
+    private var viewModel: HomeDeliveryViewModel
+    private var coordinator: HomeDeliveryCoordinator
+    
+    public init(viewModel: HomeDeliveryViewModel,
+                homeView: HomeDeliveryView,
+                coordinator: HomeDeliveryCoordinator) {
+        self.viewModel = viewModel
+        self.homeView = homeView
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildLayout()
+        loadPlaces()
+        bindActions()
+    }
+    
+    private func bindActions() {
+        homeView.onSelectedPlace = { [weak self] place in
+            self?.coordinator.navigateToPlaceDetail(place: place)
+        }
+    }
+}
+
+extension HomeDeliveryViewController {
+    func loadPlaces() {
+        homeView.startLoading()
+        viewModel.fetchPlaces { [weak self] result in
+            self?.homeView.stopLoading()
+            switch result {
+            case .success(let places):
+                self?.homeView.setup(with: places)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension HomeDeliveryViewController: ViewCodeProtocol {
+    func setViewHierarchy() {
+        self.view.addSubview(homeView)
+    }
+    
+    func setViewConstraints() {
+        homeView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setViewConfigs() {
+        self.view.backgroundColor = Color.redDark
+    }
+}
