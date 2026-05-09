836fe4eeebfcd6c77e3ce60197fe65eaa864c9ee
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-09 03:24:27 -0300

    Add AchouFood/Scenes/Order/OrderViewController.swift

---
+++ AchouFood/Scenes/Order/OrderViewController.swift
@@ -0,0 +1,68 @@
+//
+//  OrderViewController.swift
+//  AchouFood
+//
+//  Created by Arthur Rios on 05/02/26.
+//
+
+import UIKit
+import SnapKit
+
+final class OrderViewController: UIViewController {
+    
+    private var orderView: OrderView
+    private var coordinator: OrderCoordinator?
+    var place: Place?
+    
+    init(
+        orderView: OrderView,
+        coordinator: OrderCoordinator
+    ) {
+        self.orderView = orderView
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildLayout()
+        bindActions()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = false
+        orderView.setup(with: place)
+    }
+    
+    func bindActions() {
+        orderView.emptyOrderButtonTapped = { [weak self] in
+            self?.coordinator?.openMenu()
+        }
+    }
+}
+
+extension OrderViewController: ViewCodeProtocol {
+    func setViewHierarchy() {
+        view.addSubview(orderView)
+    }
+    
+    func setViewConstraints() {
+        orderView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setViewConfigs() {
+    }
+}
+
+extension OrderViewController {
+    public func setPlace(place: Place?) {
+        self.place = place
+    }
+}
