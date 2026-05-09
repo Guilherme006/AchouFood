3c6c290618c2745699f2640eb9f873270aec101a
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-09 03:22:51 -0300

    Add AchouFood/Common/Constants/Metrics/Metrics-Constants.swift

---
+++ AchouFood/Common/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Metrics-Constants.swift
+//  AchouFood
+//
+//  Created by Arthur Rios on 14/10/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    public static var huge: CGFloat = 48
+    public static var medium: CGFloat = 24
+    public static var regular: CGFloat = 20
+    public static var big: CGFloat = 16
+    public static var small: CGFloat = 12
+    public static var tiny: CGFloat = 8
+    public static var little: CGFloat = 4
+    public static var nano: CGFloat = 2
+}
